a7de860048889df5d220d9e0e1c83b3bbbad626d
Sandupa Abeywardana <74253035+SandupaAbeywardana@users.noreply.github.com>
2022-05-17 10:51:16 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,8 @@
+import matplotlib.pyplot as plt
+x = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+y = [8, 11, 14, 17, 20, 23, 26, 29, 32, 35]
+plt.plot(x, y)
+plt.ylabel('Y')
+plt.xlabel('X')
+plt.title('Y=3X+5')
+plt.show()
